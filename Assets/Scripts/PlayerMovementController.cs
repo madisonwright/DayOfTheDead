67351838bc903e851456b2bb66724eadc890e7fa
lanchuanqi <lanchuanqi1993@gmail.com>
2017-05-03 21:04:13 -0700

    Added Enemy Object and the movement script

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -8,6 +8,10 @@ public class PlayerMovementController : MonoBehaviour {
         var horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
         var vertical = CrossPlatformInputManager.GetAxis("Vertical");
 
-        transform.position += new Vector3(horizontal, 0.0f, vertical) * speed * Time.deltaTime;
+		var newPosition = transform.position;
+		newPosition += new Vector3(horizontal, 0.0f, vertical).normalized * speed * Time.deltaTime;
+		newPosition.x = Mathf.Clamp (newPosition.x, -50.0f, 50.0f);
+		newPosition.z = Mathf.Clamp (newPosition.z, -50.0f, 50.0f);
+		transform.position = newPosition;
     }
 }
