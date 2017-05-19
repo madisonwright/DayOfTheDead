e7e81ab70a3ac1270d1e0d4ce7513883ca7a1d5d
Mwright <mwrightphysics@gmail.com>
2017-05-18 19:54:45 -0700

    Added a collections prefab and tag, placed throughout the human world

---
+++ Assets/Scripts/Collections.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collections : MonoBehaviour {
+
+	private int count;
+	private AudioSource source5;
+
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Colletion")) 
+		{
+			source5.Play();
+			count += 1;
+		}
+	}
+	
+
+
+}
--- Assets/Scripts/SE_count.cs
+++ Assets/Scripts/SE_count.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class SE_count : MonoBehaviour {
 
 	private int count = 0;
+	private int count2 = 0;
     public Text spirit_energy;
+    public Text collection_energy;
     private AudioSource source;
     public GameObject particles;
+    ParticleSystem hitParticles;
+
+
+
 
     void Start(){
     	source = GetComponent<AudioSource>();
     	spirit_energy.text = "Spirit Energy: 0";
+    	collection_energy.text = "Collections: 0";
+
     }
 
 	void OnTriggerEnter(Collider other){
@@ -23,11 +31,23 @@ public class SE_count : MonoBehaviour {
 			particles.SetActive(true);
 			spirit_energy.text = "Spirit Energy: " + count.ToString();
 			StartCoroutine(_Hide());
+		}else if (other.gameObject.CompareTag ("Collection")){
+			source.Play();
+			count2 += 1;
+			collection_energy.text = "Collections: " + count2.ToString();
+	    	hitParticles = other.GetComponentInChildren <ParticleSystem> ();
+			hitParticles.Play();
+			StartCoroutine(_reward(other.gameObject));
 		}
 	}
 
 	private IEnumerator _Hide() {
             yield return new WaitForSeconds(0.5f);
 			particles.SetActive(false);
+    }
+
+    private IEnumerator _reward(GameObject other) {
+            yield return new WaitForSeconds(1.5f);
+			other.SetActive(false);
     }
 }
