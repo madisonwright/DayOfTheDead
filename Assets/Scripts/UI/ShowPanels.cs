12a2b4b879f990068078ca6f593c4bd63fc5f08f
Manish Mathai <manishpmathai@gmail.com>
2017-05-30 22:33:40 -0700

    Add menu and connect to tutorial

---
+++ Assets/Scripts/UI/ShowPanels.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowPanels : MonoBehaviour {
+
+	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
+
+
+	//Call this function to activate and display the main menu panel during the main menu
+	public void ShowMenu()
+	{
+		menuPanel.SetActive (true);
+	}
+
+	//Call this function to deactivate and hide the main menu panel during the main menu
+	public void HideMenu()
+	{
+		menuPanel.SetActive (false);
+	}
+
+    public void StartTutorial() {
+        SceneManager.LoadScene("Tutorial");
+        HideMenu();
+    }
+
+}
---
+++ Assets/Scripts/UI/StartOptions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartOptions : MonoBehaviour {
+	public string sceneToStart = "";
+
+	public Animator animColorFade;
+	public AnimationClip fadeColorAnimationClip;
+	
+	private ShowPanels showPanels;
+
+	void Awake() {
+		showPanels = GetComponent<ShowPanels> ();
+	}
+
+    public void StartButtonClicked() {
+		Invoke ("LoadDelayed", fadeColorAnimationClip.length * .5f);
+		animColorFade.SetTrigger ("fade");
+	}
+
+	public void LoadDelayed() {
+		showPanels.HideMenu ();
+		SceneManager.LoadScene (sceneToStart);
+	}
+
+	public void HideDelayed() {
+		showPanels.HideMenu();
+	}
+}
