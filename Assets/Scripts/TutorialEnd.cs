83d0d8fb61dc951e0bb786be63054a6a68863116
Mwright <mwrightphysics@gmail.com>
2017-06-05 18:53:36 -0700

    Refined tutorial and replaced human world buildings and colliders

---
+++ Assets/Scripts/TutorialEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialEnd : MonoBehaviour {
+
+	public Text move_on; 
+
+	void Start(){
+		move_on.text = "";
+	}
+
+	private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            move_on.text = "Press the Spacebar to travel to the Human world";
+        }
+    }
+}
+
