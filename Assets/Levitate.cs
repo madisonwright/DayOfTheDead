9c3c5a34b7001ba8aeeb199a63bdfbdb320a4857
Matt Rozenboim <mattrozenboim@gmail.com>
2017-06-06 18:39:11 -0700

    Added Levitate Script
    Added Levitate Script

---
+++ Assets/Levitate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levitate : MonoBehaviour {
+	public float amplitude;
+	public float velocity;
+	public float tmp;
+	public Vector3 pos;
+
+
+	// Use this for initialization
+	void Start () {
+		//tmp = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos.y = amplitude * Mathf.Sin (velocity * Time.time);
+		transform.position += pos;
+	}
+}
