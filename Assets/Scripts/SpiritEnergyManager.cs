086de34eceffd1e39d0a1e3bb6ff92d25901fa8f
Manish Mathai <manishpmathai@gmail.com>
2017-06-12 13:37:28 -0700

    Update UI energy bar each time energy changes

--- Assets/Scripts/SpiritEnergyManager.cs
+++ Assets/Scripts/SpiritEnergyManager.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SpiritEnergyManager : PersistentSingleton<SpiritEnergyManager> {
+    private float _energy = 0.0f;
     public float Energy {
-        get; set;
+        get {
+            return _energy;
+        }
+        set {
+            _energy = value;
+            var bar = GameObject.Find("Spirit Energy Bar");
+            if (bar) {
+                var slider = bar.GetComponent<Slider>();
+                slider.value = _energy;
+            }
+        }
     }
 }
