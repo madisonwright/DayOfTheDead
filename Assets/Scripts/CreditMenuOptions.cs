bdf1cfef2173a2d5422dd4ec753c6cc848f154f0
Manish Mathai <manishpmathai@gmail.com>
2017-06-12 14:53:30 -0700

    Add win menu and credits scene

---
+++ Assets/Scripts/CreditMenuOptions.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditMenuOptions : MonoBehaviour {
+    public void MainMenu() {
+        SceneManager.LoadScene("Bootstrap");
+    }
+}
--- Assets/Scripts/SE_count.cs
+++ Assets/Scripts/SE_count.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SE_count : MonoBehaviour {
-	public int count2 = 0;
+    public int count2 = 0;
     public Slider spirit_energy;
     public Text collection_energy;
     public AudioSource source;
@@ -13,15 +13,16 @@ public class SE_count : MonoBehaviour {
     ParticleSystem hitParticles;
     private int goal = 4;
 
+    public GameObject winMenu;
     //GameObject player;
     //PlayerHealth playerHealth;
 
-    void Awake(){
-		if (LevelManager.Instance.IsTutorial) {
-			this.enabled = false;
-			return;
-		}
-    	collection_energy.text = "0/?";
+    void Awake() {
+        if (LevelManager.Instance.IsTutorial) {
+            this.enabled = false;
+            return;
+        }
+        collection_energy.text = "0/?";
         //player = GameObject.FindGameObjectWithTag ("Player");
         //playerHealth = player.GetComponent<PlayerHealth>();
 
@@ -31,41 +32,49 @@ public class SE_count : MonoBehaviour {
         EventMessenger.StartListening(Events.WORLD_SWITCH_STARTED, OnWorldSwitchStarted);
     }
 
-    void OnTriggerEnter(Collider other){
-		if (other.gameObject.CompareTag ("SpiritEnergy")) 
-		{
-			source.Play();
-			if (!LevelManager.Instance.IsTutorial) {
-				particles.SetActive (true);
-				StartCoroutine (_UpdateSlider ());
-				StartCoroutine (_Hide ());
-			}
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("SpiritEnergy")) {
+            source.Play();
+            if (!LevelManager.Instance.IsTutorial) {
+                particles.SetActive(true);
+                StartCoroutine(_UpdateSlider());
+                StartCoroutine(_Hide());
+            }
 
-		}else if (other.gameObject.CompareTag ("Collection")){
-			source.Play();
-			if (!LevelManager.Instance.IsTutorial) {
-				count2 += 1;
-				collection_energy.text = count2.ToString () + "/" + goal.ToString ();
-				hitParticles = other.GetComponentInChildren <ParticleSystem> ();
-				hitParticles.Play ();
-				StartCoroutine (_reward (other.gameObject));
-			}
-		}
-	}
+        } else if (other.gameObject.CompareTag("Collection")) {
+            source.Play();
+            if (!LevelManager.Instance.IsTutorial) {
+                count2 += 1;
+                collection_energy.text = count2.ToString() + "/" + goal.ToString();
+                hitParticles = other.GetComponentInChildren<ParticleSystem>();
+                hitParticles.Play();
+                StartCoroutine(_reward(other.gameObject));
+            }
+        }
+        if (count2 == 9) {
+            StartCoroutine(_WinCondition());
+        }
+    }
 
-	private IEnumerator _Hide() {
-            yield return new WaitForSeconds(0.5f);
-			particles.SetActive(false);
+    private IEnumerator _Hide() {
+        yield return new WaitForSeconds(0.5f);
+        particles.SetActive(false);
     }
 
     private IEnumerator _reward(GameObject other) {
-            yield return new WaitForSeconds(.5f);
-			other.SetActive(false);
+        yield return new WaitForSeconds(.5f);
+        other.SetActive(false);
     }
 
     private IEnumerator _UpdateSlider() {
         yield return null;
         spirit_energy.value = SpiritEnergyManager.Instance.Energy;
+    }
+
+    private IEnumerator _WinCondition() {
+        winMenu.SetActive(true);
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene("Credits");
     }
 
     private void OnWorldSwitchStarted() {
