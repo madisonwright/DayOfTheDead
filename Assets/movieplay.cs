8f268035222f27bf519681eb325fbd1588a6c61d
Mwright <mwrightphysics@gmail.com>
2017-06-12 16:33:05 -0700

    worked on menu screen

---
+++ Assets/movieplay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+ using UnityEngine.Events;
+ 
+ public class movieplay : MonoBehaviour{
+     
+    MovieTexture mt;
+    RectTransform rt;
+    Vector2 origPos;
+    public bool loop;
+    void Awake(){
+        rt = GetComponent<RectTransform>();
+        origPos = rt.anchoredPosition;
+         
+         
+        RawImage rim = GetComponent<RawImage>();
+        mt = (MovieTexture)rim.mainTexture;
+        mt.Play();
+        mt.loop = true;
+        
+    }
+             
+}
