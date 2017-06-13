4176266d68a799826b62cccf21cd37b57325ef3f
Mwright <mwrightphysics@gmail.com>
2017-06-12 22:27:44 -0700

    ...well, I think that's it...

--- Assets/Scripts/_Player/MacheteHitDetector.cs
+++ Assets/Scripts/_Player/MacheteHitDetector.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class MacheteHitDetector : MonoBehaviour {
     public int damageAmount = 0;
     private AudioSource source;
     public AudioClip woosh;
-    
+    public ParticleSystem hitParticles;
+	public AudioSource source2;	
+
+
 	void Start()
 	{
 		source = GetComponent<AudioSource>();
+
 	}
+
+    private IEnumerator _power_move() {
+		damageAmount = 40;
+        yield return new WaitForSeconds(10.0f);
+        damageAmount = 20;
+        hitParticles.Stop();
+    }
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Enemy")) {
             var enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+			if (SpiritEnergyManager.Instance.Energy > 20){
+				source2.Play();
+				hitParticles.Play();
+				StartCoroutine(_power_move());
+				SpiritEnergyManager.Instance.Energy = 0;
+			}
+
 			if (!enemyHealth) {
 				var starEnemyHealth = other.gameObject.GetComponent<StarEnemyHealth> ();
 				source.PlayOneShot (woosh);
